c292e89c66dd6c6ee8cb45daf872d7e619cab4a6
Branislav Jenco <branislav.jenco@sesam.io>
2023-05-14 00:47:36 +0200

    Refactored a bit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # finn-enhancer
 A Chrome Extension that adds functionality when browsing realty on Finn.no
+
+TODO:
+- [x] add basic functionality
+- [] see if it's useful in any way to make the map in ArcGIS
+
--- content.js
+++ content.js
@@ -1,23 +1,28 @@
 // content.js
 
-// Create the iframe element
-const iframe = document.createElement("iframe");
 
-section = document.querySelectorAll('[data-testid="object-title"]')[0];
-address = document.querySelectorAll('[data-testid="object-address"]')[0];
+// finn.no elements with information
+top_section_query = '[data-testid="object-title"]'
+address_el_query = '[data-testid="object-address"]'
 
+// map layer urls
+stoy_url = "https://wms.miljodirektoratet.no/arcgis/rest/services/stoy/stoykart_strategisk_veg/MapServer"
+flom_url = "https://nve.geodataonline.no/arcgis/rest/services/FlomAktsomhet/MapServer"
+losmasse_url = "https://arcgis06.miljodirektoratet.no/arcgis/rest/services/NIN_hjelpelag/Losmasser/MapServer"
+kvikkleire_url = "https://nve.geodataonline.no/arcgis/rest/services/SkredKvikkleire2/MapServer"
 
-function getMap(url1, url2, address) {
-    arcgis_url = `https://www.arcgis.com/home/webmap/viewer.html?urls=${url1},${url2}&source=sd&find=${encodeURIComponent(address.innerHTML)}&mapOnly=true`
+section = document.querySelectorAll(top_section_query)[0];
+address = document.querySelectorAll(address_el_query)[0];
+
+function getMap(address, urls) {
+    arcgis_url = `https://www.arcgis.com/home/webmap/viewer.html?urls=${urls.join(",")}&source=sd&find=${encodeURIComponent(address.innerHTML)}&mapOnly=true`
     return arcgis_url
 }
 
+// Create the iframe element
+const iframe = document.createElement("iframe");
 
-stoy_url = "https%3A%2F%2Fwms.miljodirektoratet.no%2Farcgis%2Frest%2Fservices%2Fstoy%2Fstoykart_strategisk_veg%2FMapServer"
-flom_url = "https%3A%2F%2Fnve.geodataonline.no%2Farcgis%2Frest%2Fservices%2FFlomAktsomhet%2FMapServer"
-
-
-iframe.src = getMap(stoy_url, flom_url, address);
+iframe.src = getMap(address, [stoy_url, flom_url, losmasse_url, kvikkleire_url]);
 iframe.width = "100%";
 iframe.height = "500px";
 iframe.frameBorder = "0";
